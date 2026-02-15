e6cb2e70fd63d4c4853f36728300e11a3166bdd9
Shashwatrao03 <pcgaming123tyr@gmail.com>
2026-02-15 11:29:21 +0530

    day 14

---
+++ day14.c
@@ -0,0 +1,18 @@
+void rotate(int** matrix, int matrixSize, int* matrixColSize) {
+int n = matrixSize;
+int i, j, temp;
+for(i = 0; i < n; i++) {
+for(j = i + 1; j < n; j++) {
+temp = matrix[i][j];
+matrix[i][j] = matrix[j][i];
+matrix[j][i] = temp;
+        }
+    }
+for(i = 0; i < n; i++) {
+for(j = 0; j < n / 2; j++) {
+temp = matrix[i][j];
+matrix[i][j] = matrix[i][n - j - 1];
+matrix[i][n - j - 1] = temp;
+   }
+  } 
+}
