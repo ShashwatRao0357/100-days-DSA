49e134666037e5900312088b0a945b1bde025185
Shashwatrao03 <pcgaming123tyr@gmail.com>
2026-02-15 11:30:38 +0530

    day 14

--- day14.c
+++ day14.c
@@ -1,18 +1,34 @@
-void rotate(int** matrix, int matrixSize, int* matrixColSize) {
-int n = matrixSize;
-int i, j, temp;
-for(i = 0; i < n; i++) {
-for(j = i + 1; j < n; j++) {
-temp = matrix[i][j];
-matrix[i][j] = matrix[j][i];
-matrix[j][i] = temp;
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    int isIdentity = 1;
+
+    if (scanf("%d", &n) != 1) return 0;
+
+    int matrix[n][n];
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+
+            if (i == j) {
+                if (matrix[i][j] != 1) {
+                    isIdentity = 0;
+                }
+            } else {
+                if (matrix[i][j] != 0) {
+                    isIdentity = 0;
+                }
+            }
         }
     }
-for(i = 0; i < n; i++) {
-for(j = 0; j < n / 2; j++) {
-temp = matrix[i][j];
-matrix[i][j] = matrix[i][n - j - 1];
-matrix[i][n - j - 1] = temp;
-   }
-  } 
+
+    if (isIdentity) {
+        printf("Identity Matrix\n");
+    } else {
+        printf("Not an Identity Matrix\n");
+    }
+
+    return 0;
 }
